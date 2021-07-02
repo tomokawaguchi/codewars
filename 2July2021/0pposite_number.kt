c9bb8a3f2c94f32f49f0a81908b9c42eb7536be5
Tomoko Kawaguchi <tomo.k.dev@gmail.com>
2021-07-02 20:34:47 +0900

    Uploading 2July2021 kata

---
+++ 2July2021/0pposite_number.kt
@@ -0,0 +1,17 @@
+// Opposite number
+
+// Very simple, given a number, find its opposite.
+
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+fun opposite(number: Int): Int {
+  return number * -1
+}
+
+
+// Other developer's solution
+fun opposite(number: Int): Int = -number
