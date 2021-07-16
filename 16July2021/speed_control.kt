8493c203e290b690bebe2d60e11d6e106b4db235
Tomoko Kawaguchi <tomo.k.dev@gmail.com>
2021-07-16 09:31:23 +0900

    Uploading 16July2021 kata

---
+++ 16July2021/speed_control.kt
@@ -0,0 +1,37 @@
+// Speed Control
+
+// In John's car the GPS records every s seconds the distance travelled from an origin (distances are measured in an arbitrary but consistent unit). For example, below is part of a record with s = 15:
+
+// x = [0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+// The sections are:
+
+// 0.0-0.19, 0.19-0.5, 0.5-0.75, 0.75-1.0, 1.0-1.25, 1.25-1.50, 1.5-1.75, 1.75-2.0, 2.0-2.25
+// We can calculate John's average hourly speed on every section and we get:
+
+// [45.6, 74.4, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0]
+// Given s and x the task is to return as an integer the *floor* of the maximum average speed per hour obtained on the sections of x. If x length is less than or equal to 1 return 0 since the car didn't move.
+
+// Example:
+// with the above data your function gps(s, x)should return 74
+
+// Note
+// With floats it can happen that results depends on the operations order. To calculate hourly speed you can use:
+
+// (3600 * delta_distance) / s.
+
+
+package gps
+
+fun gps(s:Int, x:DoubleArray):Int {
+    var max = 0
+    for(i in 0 until x.size - 1)
+        if(max < ((3600 * (x[i+1] - x[i])) / s)) {
+            max = (3600  * (x[i+1] - x[i]) / s).toInt()
+        }
+    return max
+}
+
+
+// Other developer's solution
+fun gps(s:Int, x:DoubleArray) = if (x.size < 2) 0 else 
+    (0 until x.lastIndex).map{ (3600 * (x[it + 1] - x[it])) / s }.max()!!.toInt()
